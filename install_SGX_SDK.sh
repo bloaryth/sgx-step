c6df8843b2722f80d1bc95277ded6ec2a5c356c1
bloaryth <bloaryth@gmail.com>
2019-08-04 14:10:45 -0400

    modified to make it on Ubuntu 18.04

--- install_SGX_SDK.sh
+++ install_SGX_SDK.sh
@@ -2,23 +2,24 @@ set -e
 #git clone https://github.com/intel/linux-sgx.git
 cd linux-sgx
 ./download_prebuilt.sh
-sudo apt-get install build-essential ocaml automake autoconf libtool wget python
-sudo apt-get install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev
-sudo apt-get install build-essential python
-sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev
-sudo apt-get install alien
-#sudo alien --scripts iclsClient-1.45.449.12-1.x86_64.rpm
-#sudo dpkg -i iclsclient_1.45.449.12-2_amd64.deb
-sudo apt-get install uuid-dev libxml2-dev cmake pkg-config
-#sudo apt install ocamlbuild
+
+# sudo apt-get install build-essential ocaml automake autoconf libtool wget python # 16.04
+sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python libssl-dev - # 18.04
+
+sudo apt-get install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev -y
+sudo apt-get install build-essential python -y
+sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev -y
+sudo apt-get install alien -y
+sudo apt-get install uuid-dev libxml2-dev cmake pkg-config -y
 
 make -j`nproc`
 make sdk_install_pkg
-make psw_install_pkg
+make deb_pkg
 
 cd linux/installer/bin/
 sudo ./sgx_linux_x64_psw_*.bin
-sudo ./sgx_linux_x64_sdk_*.bin << EOF
+cd ../deb
+sudo dpkg -i ./libsgx-urts_*-*_amd64.deb ./libsgx-enclave-common_*-*_amd64.deb << EOF
 no
 /opt/intel
 EOF
